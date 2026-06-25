f8a171f3bca43eae655208af3508075b85199ff7
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-06-24 21:35:12 -0300

    Subindo arquivo novo

---
+++ 01_Concionais/Verificacao_Idade/Verificacao_Idade/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Verificacao_Idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade:");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você é maior de idade.");
+            }
+            else
+            {
+                Console.WriteLine("Você é menor de idade.");
+            }
+        }
+    }
+}
