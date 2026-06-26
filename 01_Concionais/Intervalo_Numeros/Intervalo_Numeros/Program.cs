5595a88fc8716b63bc213cc7bce54445acd2c995
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-06-26 09:26:11 -0300

    Subindo duas novas atividades (Intervalo_Numeros e Validação_Desconto)

---
+++ 01_Concionais/Intervalo_Numeros/Intervalo_Numeros/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Intervalo_Numeros 
+{
+    internal class Program 
+    { 
+        static void Main(string[] args)
+        {
+            // O do while foi adicionado para testes, deve ser considerado semente as codições if else
+            char resposta;
+            do
+            {
+                Console.WriteLine("==========================");
+                Console.WriteLine("===Intervalo de Números===");
+                Console.WriteLine("==========================");
+                Console.WriteLine();
+                Console.WriteLine();
+
+
+                Console.Write("Informe um valor inteiro: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num >= 10 && num <= 50)
+                {
+                    Console.Write("O valor está no intervalo");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write("O valor está fora do intervalo");
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+                Console.Write("Deseja testar novamente? (S/N)");
+                resposta = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.Clear();
+
+            } while (char.ToLower(resposta) != 'n');
+           
+        }
+    }
+}
---
+++ 01_Concionais/Validacao_Desconto/Validacao_Desconto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Validacao_Desconto
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine("===Validação de Descontos===");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+
+            Console.Write("Informe a o valor da compra: ");
+            float Vcompra = float.Parse(Console.ReadLine());
+
+            Console.Write("Peça o cliente para informar a sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+
+            if(Vcompra > 100 || idade >= 60)
+            {
+                float Vfinal = (Vcompra * 10) / 100;
+                Console.WriteLine($"Parabéns,você recebeu um desconto de 10% na sua compra. O valor a ser pago é: {Vfinal} ");
+            }
+            else
+            {
+                Console.WriteLine($"O valor total da sua compra é de: {Vcompra}");
+            }
+        }
+    }
+}
