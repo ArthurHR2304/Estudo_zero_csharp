2d7d545e2bdb09e092c7dc508fece9b6baeb710d
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-01 10:24:07 -0300

    Ex Ano Bissexto

---
+++ 01_Concionais/Verificador_Bissexto/Verificador_Bissexto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Verificador_Bissexto
+{
+    internal class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("==============================");
+            Console.WriteLine("===Verificando Ano Bissexto===");
+            Console.WriteLine("==============================");
+            Console.WriteLine();
+
+            Console.Write("Informe um ano: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            if (ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0))
+            {
+                Console.WriteLine($"O ano de {ano}, é um ano bissexto");
+            }
+            else
+            {
+                Console.WriteLine("O ano informador não é bissexto");
+            }
+        }
+    }
+}
