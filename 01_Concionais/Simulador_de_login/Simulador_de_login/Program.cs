a9e62e999b70a92a52884b4bde806a7ced61c21f
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-14 12:04:58 -0300

    Atividade login usuário e senha

---
+++ 01_Concionais/Simulador_de_login/Simulador_de_login/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Simulador_de_login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine("===Validação de Segurança===");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+
+            string usuário = "admin@";
+            string senha = "@1234";
+            bool validação = false;
+
+            Console.Write("Informe o seu usuário de login: ");
+            string user = Console.ReadLine();
+
+            Console.Write("Informe a sua senha de login:");
+            string pass = Console.ReadLine();
+            Console.WriteLine();
+
+            if (user == usuário && pass == senha)
+            {
+                Console.WriteLine("Olá, bem-vindo ao sistema! ");
+            }
+            else if(user != usuário && pass != senha)
+            {
+                Console.WriteLine("Usuário não encontrado: ");
+            }
+            else if (user == usuário && pass != senha)
+            {
+                Console.WriteLine($"A senha não corresponde ao usuário {usuário} ");
+            }
+        }
+    }
+}
