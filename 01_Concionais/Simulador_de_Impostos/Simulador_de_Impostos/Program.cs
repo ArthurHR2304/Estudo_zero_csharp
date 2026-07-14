058290cabff29717425d28af487c7820c69c3d01
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-14 11:29:55 -0300

    Calculando Impostos

---
+++ 01_Concionais/Simulador_de_Impostos/Simulador_de_Impostos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.X86;
+
+namespace Simulador_de_Impostos 
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("===Calculando Impostos===");
+            Console.WriteLine("=========================");
+            Console.WriteLine();
+
+            Console.Write("Informe o seu salário bruto : ");
+            double salarioBruto = double.Parse(Console.ReadLine());
+
+            double imposto = 0;
+            double salarioLiq;
+
+            if (salarioBruto <= 2000) 
+            {
+                imposto = 0;
+            }
+            else if (salarioBruto <= 4000)
+            {
+                imposto = (salarioBruto * 0.10);
+            }
+            else
+            {
+                imposto = (salarioBruto * 0.20);
+            }
+
+            salarioLiq = salarioBruto - imposto;
+
+
+            Console.WriteLine($"O valor a ser recebido é de {salarioLiq.ToString("0.00")} ");
+        }
+    }
+}
+
+
