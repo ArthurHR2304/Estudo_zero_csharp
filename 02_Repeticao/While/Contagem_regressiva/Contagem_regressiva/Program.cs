8ca3bf18a1deafa500c910a4f18ac33a1b127f4e
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-15 15:35:25 -0300

    Contagem Regressiva

---
+++ 02_Repeticao/While/Contagem_regressiva/Contagem_regressiva/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Contagem_regressiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temporizador = 10;
+
+            while (temporizador != 0 )
+            {
+               
+                Console.WriteLine($"Cronômetro: {temporizador}");
+                temporizador--;
+            }
+
+            Console.WriteLine("Bommmm!!");
+        }
+    }
+}
