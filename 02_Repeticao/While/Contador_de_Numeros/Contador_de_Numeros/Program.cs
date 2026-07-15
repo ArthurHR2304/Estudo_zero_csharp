c095622018831599ab8d01570fc45d7a0ee18876
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-15 15:22:14 -0300

    Contador de números

---
+++ 02_Repeticao/While/Contador_de_Numeros/Contador_de_Numeros/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Contador_de_Numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+
+            while (numero < 11)
+            {
+                Console.WriteLine(numero);
+                numero++; // Atualiza o contador
+               
+            }
+        }
+    }
+}
