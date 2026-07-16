66a59c26d62f8b3c689dbad8961e61336a04319d
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-16 08:59:00 -0300

    .

---
+++ 02_Repeticao/While/Contador_Personalizado/Contador_Personalizado/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Contador_Personalizado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um valor inteiro: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            int contador = 1;
+            
+            while (contador <= valor)
+            {
+                Console.WriteLine($"A contagem dos números é: {contador}");
+                contador++;
+            }
+        }
+    }
+}
