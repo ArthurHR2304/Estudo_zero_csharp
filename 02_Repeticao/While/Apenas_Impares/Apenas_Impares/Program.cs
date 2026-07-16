c0f91624a6ee5d1bfb987eb3d79d30c8d3b44ec7
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-16 08:29:03 -0300

    Apenas Ímpares

---
+++ 02_Repeticao/While/Apenas_Impares/Apenas_Impares/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Apenas_Impares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int impar = 1;
+
+            while(impar <= 20)
+            {
+                Console.WriteLine(impar);
+
+                impar += 2;
+            }
+        }
+    }
+}
