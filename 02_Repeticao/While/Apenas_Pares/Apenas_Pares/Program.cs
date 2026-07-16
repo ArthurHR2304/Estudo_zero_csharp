32a2c79ea81ebc5c1314254038f70e680c494cb4
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-16 08:25:24 -0300

    Apenas pares

---
+++ 02_Repeticao/While/Apenas_Pares/Apenas_Pares/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Apenas_Pares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pares = 2;
+
+            while(pares <= 20)
+            {
+                Console.WriteLine($"Números pares: {pares}");
+
+                pares += 2;
+            }
+        }
+    }
+}
