10bacdbaae41b05a3219567a6f191f75970f7787
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-16 08:33:20 -0300

    Cinco em Cinco

---
+++ 02_Repeticao/While/Cinco_em_Cinco/Cinco_em_Cinco/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Cinco_em_Cinco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quinto = 0;
+
+            while (quinto <= 50)
+            {
+                Console.WriteLine(quinto);
+
+                quinto += 5;
+            }
+        }
+    }
+}
