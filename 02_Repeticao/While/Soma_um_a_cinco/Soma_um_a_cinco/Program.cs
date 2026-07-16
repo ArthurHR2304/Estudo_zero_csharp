a9f4b44f03144309722404315d497e289a2ed23a
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-16 14:35:01 -0300

    .

---
+++ 02_Repeticao/While/Soma_um_a_cinco/Soma_um_a_cinco/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Soma_um_a_cinco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 5;
+            int contador = 1;
+            int soma = 0;
+
+            while (contador <= valor)
+            {
+                soma = contador + soma;
+                Console.WriteLine($"Somando o número {contador}. Total acumulado até agora: {soma}");
+                contador++;
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine($"A soma de todos os valores é: {soma}");
+        }
+    }
+}
