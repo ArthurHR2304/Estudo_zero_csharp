39253bb0321436d6e18ef2c6596e486b963b8b2a
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-17 13:43:43 -0300

    Eco de palavras

---
+++ 02_Repeticao/While/Eco_de_palavras/Eco_de_palavras/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Eco_de_palavras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fim = "parar";
+
+            Console.Write("Digite qualquer palavra: ");
+            string palavra = Console.ReadLine();
+
+            while(palavra != fim)
+            {
+                Console.WriteLine(palavra);
+
+                Console.Write("Digite outra palavra: ");
+                palavra = Console.ReadLine();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Você saiu do sistema. Parabéns!!");
+
+        }
+
+    }
+}
