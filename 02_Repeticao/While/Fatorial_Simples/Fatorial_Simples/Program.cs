c2ccbfdc5ca3b279a1c0880d3674ea6c9ea06362
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 14:07:18 -0300

    Fatorial Simples

---
+++ 02_Repeticao/While/Fatorial_Simples/Fatorial_Simples/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Fatorial_Simples
+{
+    class Progran 
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int resultado = 1;
+
+            Console.Write("Informe um valor para calcular o fatorial: ");
+            int num = int.Parse(Console.ReadLine());
+
+            while(contador <= num)
+            {
+                resultado *= contador;
+                contador++;
+            }
+
+            Console.WriteLine($"O fatorial de {num} é {resultado}");
+        }
+    }
+
+}
+
