d631e424c13dea333531d1fb9088e706844411a5
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 14:34:04 -0300

    Tabuada for

---
+++ 02_Repeticao/For/Tabuadas/Tabuadas/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Tabuadas
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Informe um valor para calcular a sua tabuada: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i < 11; ++i)
+            {
+                int resul = num * i;
+
+                Console.WriteLine($"{num} x {i} = {resul}");
+            }
+        }
+    }
+}
