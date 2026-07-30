ae06d240ca9459505abad3b083cc9a568c7867a9
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 09:19:38 -0300

    Média Simples

---
+++ 02_Repeticao/While/Media_Simples/Media_Simples/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Media_Simples
+{
+    class Progran 
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            float med = 0;
+            int div = 5;
+            float soma = 0;
+
+            while (contador < 5)
+            {
+                Console.Write($"Informe a {contador + 1}° nota: ");
+                float nota = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                soma += nota;
+                contador++; 
+            }
+
+            med = soma / 5;
+            Console.WriteLine($"A soma total dos valores é: {soma}");
+            Console.WriteLine($"A média das notas é: {med}");
+
+            
+        }
+    }
+}
