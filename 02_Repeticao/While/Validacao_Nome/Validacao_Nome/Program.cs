1a30d9ff261c5f0d94e7a7c6386c863a1f92b171
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 08:28:30 -0300

    Validador de nomes

---
+++ 02_Repeticao/While/Validacao_Nome/Validacao_Nome/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Validacao_Nome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+
+            while (nome.Length < 3)
+            {
+                
+                Console.Write("Infome um nome: ");
+                nome = Console.ReadLine();
+                
+
+                if(nome.Length >= 3)
+                {
+                    Console.WriteLine($"Isso mesmo, o nome {nome} contém mais de três letras");
+                }
+                else
+                {
+                    Console.WriteLine("Nome inválido, por favor escreva novamente!");
+                    Console.WriteLine();
+                }
+                
+            }
+        }
+    }
+}
