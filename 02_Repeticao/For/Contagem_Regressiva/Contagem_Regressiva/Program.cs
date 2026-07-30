7760e93b041a8a934f91f8fdd3fb6ccdc3ac732e
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 15:04:38 -0300

    Contagem Regressiva

---
+++ 02_Repeticao/For/Contagem_Regressiva/Contagem_Regressiva/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Contagem_Regressiva
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"Lançamento em {i}");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Fogo! 🚀 Foguete lançado!");
+        } 
+    }
+}
