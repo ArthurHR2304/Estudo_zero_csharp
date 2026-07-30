c6119d92d1390e6c12def04cffff4a9082f4b893
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 08:44:47 -0300

    Senha numerica simples

---
+++ 02_Repeticao/While/Senha_Numerica_Simples/Senha_Numerica_Simples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Senha_Numerica_Simples 
+{
+    class Progran
+    {
+        static void Main(string[] args) 
+        {
+            string senha = "1234";
+            string digito = "";
+
+            while(digito != senha)
+            {
+                Console.Write("Informe a senha cadastrada: ");
+                digito = Console.ReadLine();
+                Console.WriteLine();
+
+                if(digito == senha)
+                {
+                    Console.WriteLine("Senha correta");
+                }
+                else
+                {
+                    Console.WriteLine("Senha incorreta, favor digitar novamente!");
+                }
+            }
+        }
+    }
+}
+
