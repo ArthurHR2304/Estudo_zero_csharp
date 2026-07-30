04304954e8db6de6b7a85947321df893c23a13bb
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 12:08:06 -0300

    Contador de cliques

---
+++ 02_Repeticao/While/Contador_cliques/Contador_cliques/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Contador_cliques 
+{
+class Progran
+    {
+        static void Main(string[] args)
+        {
+            string entrada = "";
+            int clique = 0;
+
+            while(entrada != "sair")
+            {
+                Console.Write("Pressione Enter várias vezes. Ou escreva (sair) para finalizar: ");
+                entrada = Console.ReadLine();
+
+                if (entrada != "sair")
+                {
+                    clique++;
+                }
+            }
+
+            Console.WriteLine($"Número de cliques realizados: {clique}");
+        }
+    }
+}
+
