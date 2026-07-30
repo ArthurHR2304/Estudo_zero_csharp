e67763723779ea671586a6bebfe67196b98b2291
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 09:34:14 -0300

    Maior_Numero

---
+++ 02_Repeticao/While/Maior_Numero/Maior_Numero/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Maior_Numero
+{
+     class Progran
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int maior = int.MinValue;
+
+            while(contador < 5)
+            {
+                Console.Write($"Digite o {contador + 1}° número: ");
+                int num = int .Parse( Console.ReadLine() );
+
+                if (num > maior)
+                {
+                    maior = num;
+                }
+
+                contador++;
+            }
+
+            Console.WriteLine($"O maior valor digitado foi: {maior}");
+        }
+    }
+}
