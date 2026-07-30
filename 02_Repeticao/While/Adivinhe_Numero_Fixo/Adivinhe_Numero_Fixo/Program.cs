69be97b7d3d1a367a9f376ce2c254b3020c4d740
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-30 11:38:31 -0300

    Número secreto

---
+++ 02_Repeticao/While/Adivinhe_Numero_Fixo/Adivinhe_Numero_Fixo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Adivinhe_Numero_Fixo 
+{
+    class Progran 
+    {
+        static void Main(string[] args)
+        {
+            int dado = 0;
+            Random aleatorio = new Random();
+            int sorte = aleatorio.Next(1,10001);
+
+            while (sorte != dado)
+            {
+                Console.Write("Digite um número de 1 a 10.000 para adivinhar o certo: ");
+                dado = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (dado != sorte)
+                {
+                    Console.WriteLine("Ops, número errado! Por favor, tente novamente!");
+
+                    if (dado > sorte)
+                    {
+                        Console.WriteLine("O valor digitado é maior do que o número secreto");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("O valor digitado é menor do que o número secreto");
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.WriteLine($"Parabéns!! O número da sorte é {sorte}");
+        }
+    }
+}
+
