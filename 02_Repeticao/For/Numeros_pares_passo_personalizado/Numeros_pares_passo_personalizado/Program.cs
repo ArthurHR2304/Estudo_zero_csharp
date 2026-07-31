0ed0b951d149a6d615ee37a76aae1f5c07139ebb
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-31 09:06:39 -0300

    Incremento de passo 2

---
+++ 02_Repeticao/For/Numeros_pares_passo_personalizado/Numeros_pares_passo_personalizado/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Numeros_pares_passo_personalizado
+{
+    class Progran 
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 2; i < 22; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}
+
+
