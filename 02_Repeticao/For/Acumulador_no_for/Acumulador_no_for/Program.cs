8f927119bd46856ef58fb3673e08311f251c67ad
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-31 09:46:33 -0300

    calculo de média

---
+++ 02_Repeticao/For/Acumulador_no_for/Acumulador_no_for/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Acumulador_no_for
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            float soma = 0;
+            float med = 0;
+
+            for ( i = 0; i < 5; i++)
+            {
+                Console.Write($"Informe a {i + 1}° nota: ");
+                float nota = float.Parse( Console.ReadLine() );
+                Console.WriteLine();
+
+                soma += nota;
+
+            }
+
+            med = soma / 5;
+            Console.WriteLine($"A média das notas informadas é: {med}");
+        }
+    }
+}
