963920029fcc2cab93154b91e874dc6820c5c6c3
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-07-31 13:44:32 -0300

    Contando_impar_par

---
+++ 02_Repeticao/For/Contado_impar_par/Contado_impar_par/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Contando_impar_par
+{
+    class Progran 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int quantidadeImpares = 0;
+            int somaImpares = 0;
+
+            for (int i = 1; i <= num; ++i)
+            {
+                if (i % 2 != 0)
+                {
+                    quantidadeImpares ++;
+                    somaImpares += i;
+                }
+            }
+
+            Console.WriteLine($"Quantidade de números ímpares de 1 até {num}: {quantidadeImpares}");
+            Console.WriteLine($"Soma de todos os ímpares encontrados: {somaImpares}");
+
+        }
+    }
+
+}
