1b9bc044aeafac0728c934a927c9882cbc5db7d0
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-04 14:18:33 -0300

    Desafio de Array

---
+++ 03_Arrays/Primeiro_desafio_Array/Primeiro_desafio_Array/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Primeiro_desafio_Array
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int[] posicoes = new int[4];
+
+            for (int i = 0; i < posicoes.Length; i++) 
+            {
+                Console.Write($"Informe o {i + 1}º valor para o Array: ");
+                posicoes[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0;i < posicoes.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Os valor referente ao array {i + 1} é o {posicoes[i]}");
+            }
+
+        }
+    }
+}
