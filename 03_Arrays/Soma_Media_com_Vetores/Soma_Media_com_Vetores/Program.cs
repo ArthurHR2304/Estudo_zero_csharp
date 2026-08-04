51ef565d9336f06f522983507171ecd2c143b2e4
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-04 15:23:50 -0300

    Média com Arrays

---
+++ 03_Arrays/Soma_Media_com_Vetores/Soma_Media_com_Vetores/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Soma_Media_com_Vetores
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            float[] notas = new float[5];
+            float soma = 0;
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.Write($"Informe o valor da {i + 1}º nota: ");
+                notas[i] = float.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < notas.Length; i++) 
+            {
+                 soma += notas[i];
+            }
+
+            float med = soma / notas.Length ;
+
+            Console.WriteLine($"A média total do aluno é: {med}");
+        }
+    }
+}
