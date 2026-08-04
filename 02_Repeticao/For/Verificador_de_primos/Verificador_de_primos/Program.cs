f6d95adf9626224109d28bf4a329081f3f219af7
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-04 13:51:13 -0300

    Divisor de primos

---
+++ 02_Repeticao/For/Verificador_de_primos/Verificador_de_primos/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Verificador_de_primos
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um valor inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int contdiv = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0) 
+                {
+                    contdiv++;
+                }
+            }
+
+            if (contdiv == 2)
+            {
+                Console.WriteLine($"O número {num} é primo!");
+            }
+            else
+            {
+                Console.WriteLine($"O número {num} não é primo! (Ele tem {contdiv} divisores)");
+            }
+        }
+    }
+}
