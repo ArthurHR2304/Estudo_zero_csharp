9d93587dd76886628c0c48a9f780a7cb106cdac2
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-04 12:00:23 -0300

    Sequência Fibonacci

---
+++ 02_Repeticao/For/Sequencia_Fibonacci/Sequencia_Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Sequncia_Fibonacci
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 0;
+
+            Console.Write("Informe com um número inteiro quantos termos da sequências de Fibonacci você deseja executar: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                int proximo = a + b;
+                a = b;
+                b = proximo;
+
+                Console.WriteLine($"Valores da Sequência {a}");
+            }
+
+        }
+
+    }
+}
