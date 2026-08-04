c200802d9eb13d6fae36451051afb37d1865ded5
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-04 15:40:52 -0300

    Encontrando o maior valor e sua posição em um Array

---
+++ 03_Arrays/Encontrar_Maior_Valor_Posicao/Encontrar_Maior_Valor_Posicao/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Encontrar_Maior_Valor_Posicao
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[6];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Informe o {i + 1}° valor: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maior = numeros[0];
+            int posiMaior = 0;
+         
+            for (int i = 0; i < numeros.Length; i++) 
+            {
+                if (numeros[i] > maior)
+                {
+                    maior = numeros[i];
+                    posiMaior = i;
+                } 
+            }
+
+            Console.WriteLine($"O maior valor encontrado no Array é: {maior}");
+            Console.WriteLine($"E a sua posição é a {posiMaior + 1}");
+        }
+    }
+}
