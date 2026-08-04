ba57c7b643e7fc5f69adac4d05cb0196cbc33c82
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-04 16:02:12 -0300

    Inversão de valores em um array

---
+++ 03_Arrays/Invertendo_a_Oderm_Vetores/Invertendo_a_Oderm_Vetores/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Invertendo_a_Ordem_Vetores
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Informe um valor para a posição {i}°: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Posição inversa dos valores digitados:");
+
+            for (int i = numeros.Length - 1; i >= 0;i--)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+        }
+    }
+}
