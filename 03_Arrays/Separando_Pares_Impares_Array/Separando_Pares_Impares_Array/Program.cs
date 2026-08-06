10168de8945957f235a1c860606dbcc3e02b726a
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-05 22:10:20 -0300

    Subindo arquivo (separando valores pares e impares)

---
+++ 03_Arrays/Separando_Pares_Impares_Array/Separando_Pares_Impares_Array/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Separando_Pares_Impares
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int[] posi = new int[6];
+
+            for (int i = 0; i < posi.Length; i++)
+            {
+                Console.Write($"Preencha o {i}º valor: ");
+                posi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int pares = 0;
+            int impar = 0;
+
+            for (int i = 0; i < posi.Length; i++)
+            {
+                if (posi[i] % 2 == 0)
+                {
+                    pares++;
+                }
+                else
+                {
+                    impar++;
+                }
+            }
+
+            Console.WriteLine($"Estes são os valores pares digitados: {pares}");
+            Console.WriteLine($"Estes são os valores ímpares digitados: {impar}");
+
+        }
+    }
+}
