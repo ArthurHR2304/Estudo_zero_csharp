847e04869635d284f4a4f7ecef9b8b24601364f0
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-06 23:16:28 -0300

    Dobrando Vetores

---
+++ 03_Arrays/Dobrando_Valores/Dobrando_Valores/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Dobrando_Valores 
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int[] ArrayA = new int[5];
+            int[] ArrayB = new int[5];
+
+            for (int i = 0; i < ArrayA.Length; i++)
+            {
+                Console.Write($"Preencha o vetor com o {i + 1}º valor: ");
+                ArrayA[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+
+            for (int i = 0; i < ArrayB.Length; i++)
+            { 
+               ArrayB[i] = ArrayA[i] * 2;
+
+                Console.WriteLine($"Vetor A: {ArrayA[i]}  |  Vetor B (Dobro): {ArrayB[i]}");
+            }
+        }
+    }
+}
