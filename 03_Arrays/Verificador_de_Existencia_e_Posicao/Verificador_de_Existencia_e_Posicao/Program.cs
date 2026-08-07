1bc45c454c5c0a8dc7ba4261c5e23cef4597dd6c
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-06 22:34:27 -0300

    Achando o valor na posição do array

---
+++ 03_Arrays/Verificador_de_Existencia_e_Posicao/Verificador_de_Existencia_e_Posicao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Metadata;
+
+namespace Verificaror_de_Existencia_e_Posicao 
+{
+    class Progran 
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[8];
+
+            for (int i = 0; i < numeros.Length; ++i)
+            {
+                Console.Write($"Informe o {i + 1}º valor: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine();
+
+            Console.Write("Informe o valor que deseja achar: ");
+            int indice = int.Parse(Console.ReadLine());
+
+            int achado = 0;
+            bool result = false;
+
+            for (int j = 0; j < numeros.Length; ++j)
+            {
+                if (numeros[j] == indice)
+                {
+                    achado = j;
+                    result = true;
+                    break;
+                }
+            }
+
+            if (result == true)
+            {
+                Console.WriteLine($"O número desejado foi encontrado na posição: {achado + 1}");
+            }
+            else
+            {
+               Console.WriteLine ("O valor não foi encontrado!"); 
+            }
+            
+        }
+           
+    }
+}
+    
