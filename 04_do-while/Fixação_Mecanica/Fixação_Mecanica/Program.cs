03201f40cb066892fc03a83e0b994fe7145ec554
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-14 19:49:58 -0300

    Validação de senha simples com do-while

---
+++ 04_do-while/Fixação_Mecanica/Fixação_Mecanica/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Validacao_de_senha_simples
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            string senha = "12345";
+            string digito = "";
+
+            do
+            {
+                Console.Write("Informe a sua senha: ");
+                 digito = Console.ReadLine();
+
+                if(digito == senha)
+                {
+                    Console.WriteLine("Acesso liberado!");
+                }
+                else
+                {
+                    Console.WriteLine("Sua senha está incorreta! Por favor, tente novamente.");
+                    Console.WriteLine();
+                }
+
+            } while (digito != senha);
+
+
+        }
+    }
+}
