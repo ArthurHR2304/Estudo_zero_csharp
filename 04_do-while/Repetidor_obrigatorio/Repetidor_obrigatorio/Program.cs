f44d6ea546a0f261c25c7e1b1ac0766a3dd693ca
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-14 20:19:17 -0300

    Repetido obrigatorio

---
+++ 04_do-while/Repetidor_obrigatorio/Repetidor_obrigatorio/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Repetidor_obrigadorio
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            float numero = 0;
+            do
+            {
+                Console.Write("Informe qualquer valor positivo: ");
+                if (float.TryParse(Console.ReadLine(), out numero))
+                {
+                    if (numero <= 0)
+                    {
+                        Console.WriteLine("Atenção: o número precisa ser maior que zero!");
+                    }
+                }
+                else
+                {
+                   
+                    Console.WriteLine("Valor inválido, por favor difite novamente");
+                }
+            } while (numero <= 0);
+
+            Console.WriteLine("Valor posistivo detectado!");
+        }
+    }
+}
