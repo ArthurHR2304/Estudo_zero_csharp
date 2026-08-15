d7df507b1522cd5723ababcb90ff8a2d2c6683c6
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-15 16:37:33 -0300

    Somador ate diferente de zero

---
+++ 04_do-while/Somador_ate_zero/Somador_ate_zero/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Somador_ate_zero
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int numero = -1;
+            int soma = 0;
+
+            do
+            {
+                Console.Write("Informe um valor inteiro ou digite 0 para encerrar : ");
+                if(int.TryParse(Console.ReadLine(), out numero ))
+                {
+                    if (numero < 0)
+                    {
+                        Console.WriteLine("Por favor, digite um número inteiro acima de 0.");
+                    }
+                    else
+                    {
+                        soma += numero;
+                    }  
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, digite um número inteiro válido.");
+                    numero = -1; // Força a variável a NÃO ser 0 para o loop continuar!
+                }
+
+            } while (numero != 0);
+
+            Console.WriteLine($"A soma de todos os valores é de: {soma}");
+        }
+    }
+}
