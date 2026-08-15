6e6915b2f6a74b0d1a39d10be7fe6146deb44f07
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-15 20:09:11 -0300

    Verificador para perguntar ao usuário se ele deseja continuar ou não no sistema usando (S/N).

---
+++ 04_do-while/Leitor_Confirmacao/Leitor_Confirmacao/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Leitor_Confirmacao
+{
+    class Progrna
+    {
+        static void Main(string[] args)
+        {
+            string resposta = "";
+
+            do 
+            {
+                Console.Write("Deseja continuar (S/N): ");
+                resposta = Console.ReadLine()?.Trim().ToUpper();
+
+                if(resposta != "S" &&  resposta != "N")
+                {
+                    Console.WriteLine("Opção inválida! Digite apenas S ou N.");
+                }
+               
+            } while (resposta != "N");
+
+            Console.WriteLine("Programa finalizado com sucesso!");
+
+        }
+    }
+}
