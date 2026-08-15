47253f954e8e246256a712b4d1556153b295bc6c
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-14 21:22:03 -0300

    Contagem regressiva

---
+++ 04_do-while/Contagem_regressiva/Contagem_regressiva/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Contagem_regressiva
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            bool entrada = false;
+            
+
+            do
+            {
+                Console.Write("Informe um valor inteiro: ");
+
+                if (int.TryParse(Console.ReadLine(), out numero) && numero >= 0) //Tenta converter e já checa se é maior ou igual a zero
+                {
+                    entrada = true;  
+                }
+                else
+                {
+                    Console.WriteLine("Esse valor está inválido! Digite novamente.\n");
+                }
+            } while (!entrada);
+
+            Console.WriteLine();
+
+            do
+            {
+               
+                Console.WriteLine($"A redução de valores é: {numero}");
+                numero--;
+
+            } while (numero >= 0);
+
+            Console.WriteLine("Fogo!");
+        }
+    }
+}
