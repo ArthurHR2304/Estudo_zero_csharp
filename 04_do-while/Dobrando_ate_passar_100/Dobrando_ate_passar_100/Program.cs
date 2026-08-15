7405f15a84175b3fd038a66a976c1007ead1901d
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-15 17:56:49 -0300

    Dobrando até 100

---
+++ 04_do-while/Dobrando_ate_passar_100/Dobrando_ate_passar_100/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Dobrando_ate_passar_100 
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.Write("Informe um valor inteiro: ");
+            if (int.TryParse(Console.ReadLine(), out numero))
+            {
+                do
+                {
+                    numero *= 2;
+                    Console.WriteLine($"Valor Atual:{numero} ");
+
+                } while (numero <= 100);
+            }
+            else
+            {
+                Console.WriteLine("Por favor, digite um número válido!");
+            }
+
+        }
+    }
+}
