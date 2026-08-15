73bde147160d546ecd66bf6d26e94904e65a88f1
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-15 20:25:18 -0300

    Contador de números pares com uma pequena adição do contador de números ímpares

---
+++ 04_do-while/Contador_Numeros_Pares/Contador_Numeros_Pares/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace Contador_Numeros_Pares
+{
+    class Progran
+    {
+        static void Main(string[] argsa)
+        {
+            int numero = -1;
+            int pares = 0;
+            int impares = 0;
+
+            do
+            {
+                Console.WriteLine("Digite um número inteiro (ou 0 para sair): ");
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    if(numero != 0)
+                    {
+                        if (numero % 2 == 0)
+                        {
+                            pares++;
+                        }
+                        else
+                        {
+                            impares++;
+                        }
+                    }
+                }
+                else
+                {
+                    numero = -1;
+                    Console.WriteLine("Valor inválido!");
+                }
+
+            } while (numero != 0);
+
+            Console.WriteLine($"Você digitou {pares} números pares.");
+            Console.WriteLine($"Você digitou {impares} números ímpares.");
+        }
+    }
+}
