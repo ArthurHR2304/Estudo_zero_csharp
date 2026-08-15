4baecfb0a617d617a27891fd2cced887e3f11560
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-15 16:07:15 -0300

    Validador de notas

---
+++ 04_do-while/Validador_de_nota_escolar/Validador_de_nota_escolar/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Validador_de_nota_esoclar
+{
+    class Progran
+    {
+        static void Main(string[] arga)
+        {
+            float nota = -1;
+
+            do
+            {
+                Console.Write("Informe a sua nota: ");
+                if(float.TryParse(Console.ReadLine(), out nota))
+                {
+                    if(nota < 0 || nota > 10)
+                    {
+                        Console.WriteLine("Nota inválida! A nota deve ser entre 0 e 10");
+                    }
+
+
+                }
+            } while (nota < 0 || nota > 10);
+
+            Console.WriteLine($"Nota {nota} cadastrada com sucesso!");
+        }
+    }
+}
