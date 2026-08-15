2dcc28598b83f13018ed934f489da5ad316a3979
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-15 16:56:44 -0300

    Validar textos vazios

---
+++ 04_do-while/Validador_texto_vazio/Validador_texto_vazio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Validador_texto_vazio
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            string nomeUser = "";
+
+            do
+            {
+                Console.Write("Informe o seu nome de usuário: ");
+                nomeUser = Console.ReadLine()?.Trim(); //O trim retira os espaços em branco digitados pelo usuário
+
+                if (string.IsNullOrWhiteSpace(nomeUser))
+                {
+                    Console.WriteLine("O nome de usuário não pode ficar em branco!");
+                }
+            } while (string.IsNullOrWhiteSpace(nomeUser));
+
+            Console.WriteLine($"Seja bem-vindo {nomeUser}");
+        }
+    }
+}
