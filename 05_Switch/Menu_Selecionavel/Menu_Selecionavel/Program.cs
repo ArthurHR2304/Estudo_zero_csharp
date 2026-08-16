4a8f3b5fed797aa21cc6755f645ef4a6b483e04c
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-15 23:52:08 -0300

    Primeiro Switch

---
+++ 05_Switch/Menu_Selecionavel/Menu_Selecionavel/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Menu_Selecionavel
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+
+            int opcao = 0;
+            
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("|===================|");
+                Console.WriteLine("| 1 -   Ver Saldo   |");
+                Console.WriteLine("| 2 - Fazer Depósito|");
+                Console.WriteLine("| 3 -     Sair      |");
+                Console.WriteLine("|===================|");
+                Console.WriteLine();
+
+                Console.Write("Informe o número referente a opção que deseja: ");
+                if (int.TryParse(Console.ReadLine(), out opcao))
+                {
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.WriteLine("Seu saldo atual é R$ 1.000,00");
+                        break;
+
+                        case 2:
+                            Console.WriteLine("Insira o valor do depósito");
+                        break;
+
+                        case 3:
+                            Console.WriteLine("Saindo do sistema!");
+                        break;
+
+                        default:
+                            Console.WriteLine("Opção inválida!");
+                            break;
+                    }
+                }
+
+                Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                Console.ReadKey();
+
+            } while (opcao != 3);         
+        }
+    }
+}
