cb1baf6f8f858d3d46f2e31317fb20720d960760
ArthurHR2304 <arthur_resende@outlook.com.br>
2026-08-31 11:40:32 -0300

    Conversor de moedas finalizado

--- 05_Switch/Conversor_de_Moedas_0.2/Conversor_de_Moedas_0.2/Conversor_de_Moedas_0.2/Program.cs
+++ 05_Switch/Conversor_de_Moedas_0.2/Conversor_de_Moedas_0.2/Conversor_de_Moedas_0.2/Program.cs
@@ -38,38 +38,356 @@
                     switch (opcao)
                     {
                         case 1:
-                            Console.Write("Informe o valor em real disponivel: R$");
-                            if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                            do
                             {
-                                decimal usd = 5.15m;
-                                decimal contacao = real / usd;
+                                continua = "S";
+                                Console.Clear();
 
-                                Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $USD:{usd:F2} é de R${contacao:F2} ");
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real)) // Tenta converter a entrada do usuário para decimal
+                                {
+
+                                    if(real > 0)
+                                    {
+                                        decimal usd = 5.15m;
+                                        decimal contacao = real / usd;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $USD:{usd:F2} é de R${contacao:F2} "); // Exibe o resultado da conversão
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+                                   
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!"); // Informa ao usuário que a entrada não é válida
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): "); // Solicita ao usuário se deseja calcular uma nova cotação
+                                continua = Console.ReadLine()?.Trim().ToUpper();
                                 Console.WriteLine();
-                            }
-                            else
-                            {
-                                Console.Write("Esse valor fornecido não pode ser encontrado!");
-                            }
+
+                            } while (continua == "S"); 
                             break;
+
                         case 2:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal eur = 6.01m;
+                                        decimal contacao = real / eur;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $EUR:{eur:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+                                   
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 3:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal gbp = 7.03m;
+                                        decimal contacao = real / gbp;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $GBP:{gbp:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 4:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal cgf = 6.42m;
+                                        decimal contacao = real / cgf;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $CGF:{cgf:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 5:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal cad = 3.72m;
+                                        decimal contacao = real / cad;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $CAD:{cad:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 6:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal ars = 0.0034m;
+                                        decimal contacao = real / ars;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $ARS:{ars:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 7:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal cny = 0.76m;
+                                        decimal contacao = real / cny;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $CNY:{cny:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 8:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal jpy = 0.032m;
+                                        decimal contacao = real / jpy;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $JPY:{jpy:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 9:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal aud = 3.68m;
+                                        decimal contacao = real / aud;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $AUD:{aud:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 10:
+                            do
+                            {
+                                continua = "S";
+                                Console.Clear();
+
+                                Console.Write("Informe o valor em real disponivel: R$");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal real))
+                                {
+                                    if (real > 0)
+                                    {
+                                        decimal btc = 406.310m;
+                                        decimal contacao = real / btc;
+
+                                        Console.WriteLine($"O valor de R${real:F2} convertido na cotação de $BTC:{btc:F2} é de R${contacao:F2} ");
+                                        Console.WriteLine();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("O valor precisa ser maior que zero para ser convertido!");
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.Write("Esse valor fornecido não pode ser encontrado!");
+                                }
+
+                                Console.Write("Deseja calcular uma nova cotação (S/N): ");
+                                continua = Console.ReadLine()?.Trim().ToUpper();
+                                Console.WriteLine();
+
+                            } while (continua == "S");
                             break;
+
                         case 11:
                             Console.WriteLine("Saindo do Sistema!");
                             break;
@@ -87,11 +405,11 @@
 
                 if (opcao != 11)
                 {
-                    Console.WriteLine("Deseja calcular uma nova cotação (S/N): ");
+                    Console.Write("Deseja voltar ao menu principal (S/N): ");
                     continua = Console.ReadLine()?.Trim().ToUpper();
 
                 }
-            } while (continua == "S" && opcao != 8);
+            } while (continua == "S" && opcao != 11);
 
             if (continua == "N")
             {
